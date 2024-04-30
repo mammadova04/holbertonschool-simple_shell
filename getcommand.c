94159cb24a2ba1a804e8ed6e9ba6e46c4148c817
mammadova04 <nmammadova04@gmail.com>
2024-04-30 13:22:07 +0000

    edited shell.c file

--- getcommand.c
+++ getcommand.c
@@ -1,51 +1,68 @@
+#include <errno.h>
 #include "main.h"
+
 /**
-  * arg_counter - counts number of arguments
-  * @buf: user input
-  * @size: size of user input
-  * Return: number of argumnets
-  */
+ * arg_counter - counts number of arguments
+ * @buf: user input
+ * @size: size of user input
+ * Return: number of arguments
+ */
 int arg_counter(char *buf, int size)
 {
 	int count = 0, i;
 
 	for (i = 1; i < size; i++)
-		count += ((buf[i - 1] == ' ' && buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '));
+		count += ((buf[i - 1] == ' ' &&  buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '));
 	return (count);
 }
+
 /**
-  * get_command - format command line arguments
-  * Return: memory address where arguments are stored
-  */
+ * get_command - format command line arguments
+ * Return: memory address where arguments are stored
+ */
 char **get_command(void)
 {
 	char **array, *buf;
 	size_t n = 0, k, i, l = 0;
 
+	buf = malloc(sizeof(char) * 1024); // Allocating memory for buf
+	if (buf == NULL)
+		return (NULL);
+
 	k = getline(&buf, &n, stdin);
 	if (k == (size_t)(-1))
 	{
+		free(buf); // Freeing allocated memory
+		if (errno == 0)
+			return (NULL);
 		perror("Failure to read line");
 		return (NULL);
 	}
-	array = malloc(sizeof(char *) * arg_counter(buf, k));
-	for (i = 1; i < size; i++)
+
+	array = malloc(sizeof(char *) * (arg_counter(buf, k) + 1));
+	if (array == NULL)
 	{
-		if ((buf[i - 1] == ' ' && buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '))
+		free(buf); // Freeing allocated memory
+		return (NULL);
+	}
+
+	for (i = 1; i < k; i++)
+	{
+		if (((buf[i - 1] == ' ' || buf[i - 1] == '\0') && buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '))
 		{
-			array[l] = buf + i;
-			while (buf[i] != ' ' && i < size && buf[i] != '\n')
+			if (buf[i - 1] == ' ' || buf[i - 1] == '\0')
+				array[l] = buf + i;
+			else
+				array[l] = buf + i - 1;
+			while (buf[i] != ' ' && i < k && buf[i] != '\n')
 				i++;
 			buf[i] = '\0';
 			l++;
 		}
 	}
+
+	array[l] = NULL;
+
 	return (array);
 }
 
-
-int main(void)
-{
-	char **array = get_command();
-	printf("%s\n%s\n", array[0], array[1]);
-}
--- shell.c
+++ shell.c
@@ -1,42 +1,43 @@
 #include "main.h"
 #include <errno.h>
-
 /**
- * main - shell start function
- * @argv: argument variables
- * @argc: argument counter
- * Return: always 0
- */
-int main(int argc, char *argv[])
+  * main - shell start function
+  * @argv: argument variables
+  * @argc: argument counter
+  * Return: always 0
+  */
+int main(int argc, char **argv)
 {
-	char *command, **fcommand = malloc(sizeof(char *) * 2);
-	size_t n = 0, k;
+	char **fcommand;
+	size_t n = 0;
 	pid_t pid;
 	struct stat st;
-
-	while(1)
+	int status;
+	while (1)
 	{
-		printf("#cisfun$ ");
-		k = getline(&command, &n, stdin);
-		if(k == (size_t)(-1))
+		status = isatty(STDIN_FILENO);
+		if (status)
+			printf("#cisfun$ ");
+		else
+			errno = 0;
+		fcommand = get_command();
+		if (fcommand == NULL)
 		{
-			perror("Failure to read line");
+			printf("\n");
 			break;
 		}
-		command[k] = '\0';
-		if (stat(command, &st) == -1)
+		if (stat(fcommand[0], &st) == -1)
 		{
-			printf("%s\n", command);
 			perror(argv[0]);
 			continue;
 		}
 		pid = fork();
 		if (pid == 0)
 		{
-			fcommand = (char *[]){command, NULL};
-			execve(command, fcommand, NULL);
+			execve(fcommand[0], fcommand, environ);
 		}
 		else
 			wait(NULL);
 	}
-} 
+	return (0);
+}
