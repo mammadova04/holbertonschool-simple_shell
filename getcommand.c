fd3f5c2670f22c36101ff510db5c53c082c833e8
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-01 20:01:36 +0000

    modified getcommand.c file

--- getcommand.c
+++ getcommand.c
@@ -1,67 +1,67 @@
 #include <errno.h>
 #include "main.h"
-
 /**
- * arg_counter - counts number of arguments
- * @buf: user input
- * @size: size of user input
- * Return: number of arguments
- */
+  * arg_counter - counts number of arguments
+  * @buf: user input
+  * @size: size of user input
+  * Return: number of argumnets
+  */
 int arg_counter(char *buf, int size)
 {
 	int count = 0, i;
 
 	for (i = 1; i < size; i++)
-		count += ((buf[i - 1] == ' ' &&  buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '));
+		count += ((buf[i - 1] == ' ' &&
+					(buf[i] != ' ' && buf[i] != '\n')) ||
+				(i == 1 &&
+				 buf[i - 1] != ' '));
 	return (count);
 }
-
 /**
- * get_command - format command line arguments
- * Return: memory address where arguments are stored
- */
-char **get_command(void)
+  * get_command - format command line arguments
+  * @buf: buffer for storing user input
+  * Return: memory address where arguments are stored
+  */
+char **get_command(char **buf)
 {
-	char **array, *buf;
-	size_t n = 0, k, i, l = 0;
+	char **array;
+	size_t n = 1, k, i, l = 0, count;
 
-	buf = malloc(sizeof(char) * 1024); // Allocating memory for buf
-	if (buf == NULL)
-		return (NULL);
-
-	k = getline(&buf, &n, stdin);
+	*buf = malloc(n), k = getline(buf, &n, stdin);
 	if (k == (size_t)(-1))
 	{
-		free(buf); // Freeing allocated memory
 		if (errno == 0)
 			return (NULL);
 		perror("Failure to read line");
 		return (NULL);
 	}
-
-	array = malloc(sizeof(char *) * (arg_counter(buf, k) + 1));
+	count = arg_counter(*buf, k);
+	if (!count)
+	{
+		array = malloc(sizeof(char *));
+		array[0] = strdup(" ");
+		return (array);
+	}
+	array = malloc(sizeof(char *) * (count + 1));
 	if (array == NULL)
 	{
-		free(buf); // Freeing allocated memory
+		free(*buf), free(array);
 		return (NULL);
 	}
-
 	for (i = 1; i < k; i++)
 	{
-		if (((buf[i - 1] == ' ' || buf[i - 1] == '\0') && buf[i] != ' ') || (i == 1 && buf[i - 1] != ' '))
+		if ((*buf)[i - 1] == ' ' || (*buf)[i - 1] == '\t' || (*buf)[i - 1] == '\n')
+			(*buf)[i - 1] = '\0';
+		else if ((*buf)[i - 1] == '\0')
+			continue;
+		else
 		{
-			if (buf[i - 1] == ' ' || buf[i - 1] == '\0')
-				array[l] = buf + i;
-			else
-				array[l] = buf + i - 1;
-			while (buf[i] != ' ' && i < k && buf[i] != '\n')
+			array[l] = *buf + i - 1;
+			while ((*buf)[i] != ' ' && (*buf)[i] != '\t' && (*buf)[i] != '\n')
 				i++;
-			buf[i] = '\0';
-			l++;
+			(*buf)[i] = '\0', l++;
 		}
 	}
-
 	array[l] = NULL;
 	return (array);
 }
-
