df3949ec0025d99ba56c743250ac61be05d06bc6
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-01 18:40:08 +0000

    strtok file created

---
+++ _strtok.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+char **_strtok(char *str, char delim)
+{
+	char **array, *start, *end;
+	int i = 0, j = 1, f = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] != delim)
+		{
+			start = str + i;
+			while (str[i] != delim && str[i])
+				i++;
+			if (j == 1)
+				array = malloc(sizeof(char *) * j), f++;
+			else
+				array = realloc(array, sizeof(char *) * j);
+			if (str[i] == '\0')
+			{
+				array[j - 1] = strdup(start);
+				j++;
+				break;
+			}
+			else
+			{
+				end = str + i;
+				*end = '\0';
+				array[j - 1] = strdup(start), *end = delim, start = end;
+			}
+		}
+	}
+	array[j - 1] = NULL;
+	return (array);
+}
