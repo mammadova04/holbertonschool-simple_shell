c859e8d62ebb9e762a83eb4e06a2f19ccc5cd9e4
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-01 18:45:16 +0000

    added find_path function to init_path file

--- init_path.c
+++ init_path.c
@@ -1,5 +1,32 @@
 #include "main.h"
 /**
+  * find_path - find path in env variables
+  * @var: env variable
+  * @i: row index
+  * @j: column index
+  */
+void find_path(char **var, int *i, int *j)
+{
+	while (var[*i])
+	{
+		while (var[*i][*j])
+		{
+			if (var[*i][*j] == PATH[*j])
+			{
+				if (var[*i][*j] == '=')
+					return;
+				(*j)++;
+			}
+			else
+				break;
+		}
+		(*i)++;
+		*j = 0;
+	}
+	*i = 0;
+	*j = 0;
+}
+/**
   * get_path - init path array
   * @env: environment variables array pointer
   * Return: path array
