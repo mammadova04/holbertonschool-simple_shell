c257061e47fc975ff593782dd1bafc174f404105
mammadova04 <nmammadova04@gmail.com>
2024-05-02 17:03:45 +0000

    modified file

--- _strtok.c
+++ _strtok.c
@@ -1,64 +1,48 @@
 #include "main.h"
-
 /**
- * _strtok - Custom strtok implementation
- * @str: String for tokenization
- * @delim: Delimiter
- *
- * Return: Array of tokens
- */
+  * _strtok - my strtok implementation
+  * @str: string for tokenization
+  * @delim: delimiter
+  * Return: array of tokens
+  */
 char **_strtok(char *str, char delim)
 {
-    char **array, *start, *end;
-    int i = 0, j = 1, num_tokens = 0;
+	char **array, *start, *end;
+	int i = 0, j = 1, f = 0;
 
-    if (str == NULL)
-        return (NULL);
-
-    while (str[i])
-    {
-        if (str[i] != delim)
-        {
-            num_tokens++;
-            while (str[i] != delim && str[i])
-                i++;
-        }
-        else
-        {
-            i++;
-        }
-    }
-
-    if (num_tokens == 0)
-        array = malloc(sizeof(char *));
-    else
-        array = malloc(sizeof(char *) * (num_tokens + 1));
-
-    if (array == NULL)
-        return (NULL);
-
-    i = 0;
-
-    while (str[i])
-    {
-        if (str[i] != delim)
-        {
-            start = str + i;
-            while (str[i] != delim && str[i])
-                i++;
-            end = str + i;
-            *end = '\0';
-            array[j - 1] = strdup(start);
-            *end = delim;
-            j++;
-        }
-        else
-        {
-            i++;
-        }
-    }
-
-    array[j - 1] = NULL;
-    return (array);
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] != delim)
+		{
+			start = str + i;
+			while (str[i] != delim && str[i])
+				i++;
+			if (j == 1)
+				array = malloc(sizeof(char *) * j), f++;
+			else
+				array = realloc(array, sizeof(char *) * j);
+			if (str[i] == '\0')
+			{
+				array[j - 1] = strdup(start);
+				j++;
+				break;
+			}
+			else
+			{
+				end = str + i;
+				*end = '\0';
+				array[j - 1] = strdup(start), *end = delim, start = end;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (f == 0)
+		array = malloc(sizeof(char *));
+	else
+		array = realloc(array, sizeof(char *) * j);
+	array[j - 1] = NULL;
+	return (array);
 }
-
