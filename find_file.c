0ebbbc8e04a64d6cb609c8851035a89e56f096ca
mammadova04 <nmammadova04@gmail.com>
2024-05-02 16:50:53 +0000

    removed realloc

--- find_file.c
+++ find_file.c
@@ -1,36 +1,50 @@
 #include "main.h"
+
 /**
-  * find_file - finds file location in path variables
-  * @command: user input
-  * @lk: is the flag for is this command use path or not
-  * Return: modified user input
-  */
+ * find_file - finds file location in path variables
+ * @command: user input
+ * @lk: is the flag for is this command use path or not
+ * Return: modified user input
+ */
 char *find_file(char *command, int *lk)
 {
-	int i = 0, len;
-	char *temp;
-	struct stat st;
+    int i = 0, len;
+    char *temp, *result = NULL;
+    struct stat st;
 
-	if (command[0] == '/' || command[0] == '.')
-		if (!stat(command, &st))
-			return (command);
-	if (!path_var)
-		return (NULL);
-	while (path_var[i])
-	{
-		len = strlen(path_var[i]);
-		temp = strdup(path_var[i]);
-		temp = realloc(temp, len + strlen(command) + 2);
-		temp[len] = '/', temp[len + 1] = '\0';
-		temp = strcat(temp, command);
-		if (!stat(temp, &st))
-		{
-			(*lk)++;
-			errno = 0;
-			return (temp);
-		}
-		i++;
-		free(temp);
-	}
-	return (NULL);
+    if (command[0] == '/' || command[0] == '.')
+    {
+        if (!stat(command, &st))
+        {
+            result = strdup(command);
+            if (result != NULL)
+                (*lk)++;
+        }
+        return (result);
+    }
+
+    if (!path_var)
+        return (NULL);
+
+    while (path_var[i])
+    {
+        len = strlen(path_var[i]);
+        temp = malloc(len + strlen(command) + 2);
+        if (temp == NULL)
+            return (NULL);
+        strcpy(temp, path_var[i]);
+        strcat(temp, "/");
+        strcat(temp, command);
+        if (!stat(temp, &st))
+        {
+            (*lk)++;
+            errno = 0;
+            result = temp;
+            break;
+        }
+        i++;
+        free(temp);
+    }
+    return (result);
 }
+
