9b41c8dda37b15de6017675082a4056cab2bee70
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-05-03 19:07:09 +0000

    fixed betty

--- find_file.c
+++ find_file.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-/**
- * path_var - Array of path variables.
- */
 char **path_var;
 
 /**
@@ -15,46 +12,41 @@ char **path_var;
  */
 char *find_file(char *command, int *lk)
 {
-    int i = 0, max_len = 0, j;
-    char *temp, *result = NULL;
-    struct stat st;
+	int i = 0, max_len = 0, j;
+	char *temp, *result = NULL;
+	struct stat st;
 
-    if (command[0] == '/' || command[0] == '.')
-    {
-        if (!stat(command, &st))
-        {
-            result = strdup(command);
-            if (result != NULL)
-                (*lk)++;
-        }
-        return (result);
-    }
-
-    if (!path_var)
-        return (NULL);
-
-    for (j = 0; path_var[j] != NULL; j++)
-        max_len += strlen(path_var[j]) + strlen(command) + 2;
-
-    temp = malloc(max_len);
-    if (temp == NULL)
-        return (NULL);
-
-    while (path_var[i])
-    {
-        strcpy(temp, path_var[i]);
-        strcat(temp, "/");
-        strcat(temp, command);
-        if (!stat(temp, &st))
-        {
-            (*lk)++;
-            errno = 0;
-            result = strdup(temp);
-            break;
-        }
-	i++;
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (!stat(command, &st))
+		{
+			result = strdup(command);
+			if (result != NULL)
+				(*lk)++;
+		}
+		return (result);
 	}
-
+	if (!path_var)
+		return (NULL);
+	for (j = 0; path_var[j] != NULL; j++)
+		max_len += strlen(path_var[j]) + strlen(command) + 2;
+	temp = malloc(max_len);
+	if (temp == NULL)
+		return (NULL);
+	while (path_var[i])
+	{
+		strcpy(temp, path_var[i]);
+		strcat(temp, "/");
+		strcat(temp, command);
+		if (!stat(temp, &st))
+		{
+			(*lk)++;
+			errno = 0;
+			result = strdup(temp);
+			break;
+		}
+		i++;
+	}
 	free(temp);
 	return (result);
 }
